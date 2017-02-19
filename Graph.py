5bba8caf315ba2d2c98ebb3fc738bdd82166dcee
Adam Summerville <adamsumm@gmail.com>
2017-02-18 16:30:19 -0800

    First commit

---
+++ Graph.py
@@ -0,0 +1,244 @@
+class Vertex:
+    def __init__(self,id=0,label=''):
+        self.label = label
+        self.id = id
+    def __str__(self):
+        return str(self.label) + str(self.id)
+    
+class Edge:
+    def __init__(self,v1,v2,label=''):
+        self.v1 = v1
+        self.v2 = v2
+        self.label = label
+    def __str__(self):
+        return str(self.v1) + '-'+str(self.label)+'->'+str(self.v2)
+class Graph:
+    
+    
+    def __init__(self,vertices,edges):
+        self.vertices = [v for v in vertices]
+        self.edges = [e for e in edges]
+        for e in edges:
+            if e.v1 not in self.vertices:
+                self.vertices.append(e.v1)
+            if e.v2 not in self.vertices:
+                self.vertices.append(e.v2)
+            
+        self.v2e = {v:{v2:set() for v2 in vertices} for v in vertices}
+        for e in edges:
+            self.v2e[e.v1][e.v2].add(e)
+
+    def renumber(self):
+        for i,v in enumerate(self.vertices):
+            v.id = i
+            
+    def add_vertex(self,v):
+        self.vertices.append(v)
+        self.v2e[v] = {v2:set() for v2 in self.vertices}
+        for o in self.vertices:
+            self.v2e[v][o] = set()
+            self.v2e[o][v] = set()
+    
+    def add_edge(self,v1,v2,label):
+        self.edges.append(Edge(v1,v2,label))
+        self.v2e[v1][v2].add(self.edges[-1])
+    def clone(self):
+        mapping = {}
+        vertices = []
+        for v in self.vertices:
+            vertices.append(Vertex(v.id,v.label))
+            mapping[v] =vertices[-1]
+        edges = []
+        for e in self.edges:
+            edges.append(Edge(mapping[e.v1],mapping[e.v2],e.label))
+        return Graph(vertices,edges)
+        
+    def find_vertex(self,id):
+        for v in self.vertices:
+            if v.id == id:
+                return v
+        return None
+    def remove_vertex(self,v,prune=True):
+        self.vertices.remove(v)
+
+        into = []
+        out_from = []
+        if not prune:
+            for o in self.v2e[v]:
+                for e in self.v2e[v][o]:
+                    out_from.append(e)
+            for o in self.v2e:
+                for e in self.v2e[o][v]:
+                    into.append(e)
+        
+            
+        del self.v2e[v]
+        for o in self.v2e:
+            del self.v2e[o][v]
+
+        if not prune:
+            for e_i in into:
+                for e_o in out_from:
+                    already_exists = False
+                    for e in self.v2e[e_i.v1][e_o.v2]:
+                        if e.label == e_o.label:
+                            already_exists = True
+                            break
+                    if not already_exists:
+                        self.add_edge(e_i.v1,e_o.v2,e_o.label)
+        to_remove = []
+        for e in self.edges:
+            if v == e.v1 or v == e.v2:
+                to_remove.append(e)
+        for e in to_remove:
+            if e in self.edges:
+                self.edges.remove(e)
+
+        to_remove = set([])
+        for ii in range(len(self.edges)):
+            e1 = self.edges[ii]
+            for jj in range(ii+1,len(self.edges)):
+                e2 = self.edges[jj]
+                if e1.label == e2.label and e1.v1 == e2.v1 and e1.v2 == e2.v2:
+                    to_remove.add(e2)
+                    
+        for e in to_remove:
+            if e in self.edges and e in self.v2e[e.v1][e.v2]:
+                self.v2e[e.v1][e.v2].remove(e)
+                self.edges.remove(e)
+    def __str__(self):
+        st = ''
+        for v in self.vertices:
+            st += 'vertex: '+str(v) + '\n'
+        for e in self.edges:
+            st += 'edge: ' +str(e) + '\n'
+        return st
+    def find_matches(self,other,Vertex_equals=None,edge_equals=None):
+        import itertools
+        if len(self.vertices) < len(other.vertices):
+            #print 'len bad'
+            return []
+        matches = []
+        
+        if not Vertex_equals:
+            Vertex_equals = lambda x,y: x.label == y.label
+        if not edge_equals:
+            edge_equals = lambda x,y: (x.label == y.label and x.v1.label == y.v1.label  and x.v2.label == y.v2.label)
+            
+        for perm in itertools.permutations(self.vertices,len(other.vertices)):
+            is_good = True
+            
+            for ind,v in enumerate(perm):
+                #print ind,v
+                if not Vertex_equals(other.vertices[ind],v):
+                    #print 'Nope'
+                    is_good = False
+                    break
+            if not is_good:
+                continue
+            is_good = len(other.edges) == 0
+            mapping = {other.vertices[i]:v for i,v in enumerate(perm)}
+            taken = set([])
+            for edge in other.edges:
+                o1 = mapping[edge.v1]
+                o2 = mapping[edge.v2]
+                is_good = False
+                for e in self.v2e[o1][o2]:
+                    
+                    if e not in taken and edge_equals(edge,e):
+                        #print 'G:',edge
+                        #print 'SG:',e
+                        taken.add(e)
+                        is_good = True
+                        break
+                if not is_good:
+                    break
+                    
+            if is_good:
+                matches.append(perm)
+                    
+        return matches
+    def to_dot(self):
+        outstr = ['digraph {']
+        mapping = {}
+        for i,v in enumerate(self.vertices):
+            mapping[v] = i
+            outstr.append('{} [label="{}"]'.format(i,v.label))
+        for e in self.edges:
+            outstr.append('{} -> {} [label="{}"]'.format(mapping[e.v1],mapping[e.v2],e.label))
+        outstr.append('}')
+        return '\n'.join(outstr)
+            
+class Rule:
+    def __init__(self,lhs,rhs,lhs_to_rhs,prune=True):
+        self.lhs = lhs
+        self.rhs = rhs
+        self.prune = prune
+        self.to_delete = [v for v in lhs.vertices if v not in lhs_to_rhs]
+        self.lhs_to_rhs = lhs_to_rhs
+        self.rhs_to_lhs = {r:l for l,r in lhs_to_rhs.items()}
+
+        self.to_add = [v for v in rhs.vertices if v not in self.rhs_to_lhs]
+        
+        self.edges_to_delete = [e for e in lhs.edges if e not in lhs_to_rhs]
+        
+        self.edges_to_add = [e for e in rhs.edges if e not in self.rhs_to_lhs]
+        
+    def __str__(self):
+        return 'RULE:\n' + str(self.lhs) + '\n=>\n'+ str(self.rhs) + '\n'
+    def find_matches(self,other,Vertex_equals=None,edge_equals=None):
+        return other.find_matches(self.lhs,Vertex_equals,edge_equals)
+    def apply_to(self,graph,match,Vertex_equals=None,edge_equals=None):
+        #print(self)
+        mapping = {self.lhs.vertices[i]:v for i,v in enumerate(match)}
+        for l,r in self.lhs_to_rhs.items():
+            print 'lhs', l, ' => ', r
+        rhs_to_new = {}
+        for v in self.to_add:
+            new_v = Vertex(label=v.label)
+            print 'adding',new_v
+            rhs_to_new[v] = new_v 
+            graph.add_vertex(new_v)
+        
+        for e in self.edges_to_add:
+            v1 = e.v1
+            if v1 in self.rhs_to_lhs:
+                v1 = mapping[self.rhs_to_lhs[v1]]
+            else:
+                v1 = rhs_to_new[v1]
+            v2 = e.v2
+            if v2 in self.rhs_to_lhs:
+                v2 = mapping[self.rhs_to_lhs[v2]]
+            else:
+                v2 = rhs_to_new[v2]
+            #new_e = Edge()
+            print 'adding ',v1, "-{}->".format(e.label),v2
+            graph.add_edge(v1,v2,e.label)
+                
+            
+        
+        for e in self.edges_to_delete:
+            print 'deleting',e
+            v1 = mapping[e.v1]
+            v2 = mapping[e.v2]
+            
+            for oe in graph.v2e[v1][v2]:
+                if oe.label == e.label:
+                    graph.edges.remove(oe)
+                    graph.v2e[v1][v2].remove(oe)
+                    break
+                
+        for v in self.to_delete:
+            print 'deleting',v
+            graph.remove_vertex(mapping[v],self.prune)
+            
+        for v in self.lhs_to_rhs:
+            print 'lhs->rhs',v,self.lhs_to_rhs[v]
+            if v in self.lhs.vertices:
+                graph.vertices[graph.vertices.index(mapping[v])].label = self.lhs_to_rhs[v].label
+            if v in self.lhs.edges:
+                for oe in graph.v2e[mapping[v.v1]][mapping[v.v2]]:
+                    if oe.label == v.label:
+                        break                        
+                oe.label =  self.lhs_to_rhs[v].label 
+        
---
+++ Parser.py
@@ -0,0 +1,275 @@
+import logging
+import re
+import sys
+
+from Graph import *
+from Lexer import Lexer
+from Token import TokenTypes
+from Token import Token
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+class Parser(object):
+    """
+    Graph productions parser.
+    """
+
+    #--------------------------------------------------------------------------
+    def __init__(self, lexer):
+        """
+        Constructor.
+        Inputs: lexer -- instance of the Lexer class
+        Outputs: N/A
+        """
+        self.lexer = lexer
+        self.lookahead = self.lexer.nextToken() # next token
+        self.config = {} # configuration section of the input
+        self.productions = [] # array of Production objects
+        self.startGraph = None # starting graph
+
+        # As we are parsing a graph, we keep track of the number
+        # of vertices parsed so far.
+        self._numVerticesParsed = 0
+         
+    #--------------------------------------------------------------------------
+    def parse(self):
+        """grammar_file -> configuration productions."""
+        self._parseConfiguration()
+        self._parseProductions()
+
+    #--------------------------------------------------------------------------
+    # PRIVATE METHODS - These aren't the methods you're looking for.
+    #--------------------------------------------------------------------------
+    def _consume(self):
+        """
+        Consumes the current token and advances to the next.
+        """
+        self.lookahead = self.lexer.nextToken()
+        
+    #--------------------------------------------------------------------------
+    def _error(self, str):
+        """
+        Raises an error message about expecting str but found current token
+        on the current line number.
+        """
+        raise SyntaxError("Expecting %s found %s on line %d" % \
+            (str, self.lookahead, self.lexer.lineNum))
+
+    #--------------------------------------------------------------------------
+    def _match(self, tokenType):
+        """
+        If the type of the current token type matches the given tokenType,
+        advance to the next token, otherwise, raise an error.
+        Inputs: tokenType is an integer from lexer.TokenTypes
+        Outputs: current token (the one just consumed)
+        """
+        if self.lookahead.type == tokenType:
+            oldToken = self.lookahead
+            self._consume()
+            return oldToken
+        else:
+            self._error(TokenTypes.names[tokenType])
+
+    #--------------------------------------------------------------------------
+    def _parseConfig(self):
+        """
+        config -> ID '=' (ID | NUMBER)
+        
+        
+        A config is a simple name/value pair.
+        """
+        key = self._match(TokenTypes.ID)
+        self._match(TokenTypes.EQUALS)
+        if self.lookahead.type == TokenTypes.ID:
+            value = self._match(TokenTypes.ID)
+        elif self.lookahead.type == TokenTypes.NUMBER:
+            value = self._match(TokenTypes.NUMBER)
+        else:
+            self._error("ID or NUMBER")
+
+        # Store the <key,value> pair in the configuration.
+        self.config[key.text] = value.text
+
+    #--------------------------------------------------------------------------
+    def _parseConfigList(self):
+        """
+        config_list -> config ';' config_list | nil
+        
+        A list of zero or more configuration statements.
+        """
+        while self.lookahead.type == TokenTypes.ID:
+            self._parseConfig()
+            self._match(TokenTypes.SEMICOLON)
+
+    #--------------------------------------------------------------------------
+    def _parseConfiguration(self):
+        """
+        configuration -> 'configuration' '{' config_list '}'
+        
+        The configuration section of the input file. Consists of a
+        'configuration' keyword, and then a list of configuration statements
+        surrounded by curly braces.
+        """
+        self._match(TokenTypes.CONFIGURATION)
+        self._match(TokenTypes.LBRACE)
+        self._parseConfigList()
+        self._match(TokenTypes.RBRACE)
+
+    #--------------------------------------------------------------------------
+    def _parseEdgeList(self, graph):
+        """
+        edgeList -> ID | ID '->' edgeList
+
+        An edgeList represents a set of graph nodes connected with directed
+        edges. In an edgeList ID is actually the label to be applied to
+        a given vertex. So A->B represents two vertices with labels "A" and
+        "B" respectively, connected by an edge from A to B.
+
+        This method reads a list of labels (ID) and edges and adds them to
+        the given graph. If the graph already has a vertex with the given
+        label, the existing vertex is used intead.
+
+        If new vertices are added they are given a unique id of "vN" where
+        N is the next available vertex number (starting with 0).
+
+        Inputs: graph - Graph to add vertices to
+        Outputs: none
+        """
+
+        currentVertexToken = self._match(TokenTypes.ID)
+        currentVertex = self._parseVertexID(currentVertexToken, graph)
+
+        while self.lookahead.type in set([TokenTypes.ARROW,TokenTypes.ARROW_START]):
+            if self.lookahead.type == TokenTypes.ARROW:
+                self._match(TokenTypes.ARROW)
+                edge_label = ''
+            else:
+                self._match(TokenTypes.ARROW_START)
+                edge_label = self._match(TokenTypes.ID).text
+                self._match(TokenTypes.ARROW_END)
+                
+            # Parse the next vertex in the input, creating a new vertex
+            # if needed.
+            nextVertexToken = self._match(TokenTypes.ID)
+            nextVertex = self._parseVertexID(nextVertexToken, graph)
+
+            # Connect the first vertex we read with the second one.
+            graph.add_edge(currentVertex, nextVertex,edge_label)
+
+            currentVertex = nextVertex
+
+    #--------------------------------------------------------------------------
+    def _parseGraph(self):
+        """
+        graph -> edgeList | edgeList ',' graph
+
+        An edgeList represents a connected path of vertices. A "graph" is
+        a comma-separated list of such paths. This method builds a graph
+        from the given representation.
+        Input: none
+        Output: Graph build from the incoming list of edgeLists
+        """
+        #logging.debug('parsing new graph')
+        g = Graph([],[])
+        self._parseEdgeList(g)
+        while self.lookahead.type == TokenTypes.COMMA:
+            self._match(TokenTypes.COMMA)
+            self._parseEdgeList(g)
+        #g.renumber()
+        return g
+
+    #--------------------------------------------------------------------------
+    def _parseProduction(self):
+        """
+        prod -> graph '==>' graph
+
+        A production defines a transformation taking one graph (on the LHS)
+        and transforming it to a different graph (RHS).
+        """
+        lhs = self._parseGraph()
+        self._match(TokenTypes.DOUBLEARROW)
+        rhs = self._parseGraph()
+        lhs_to_rhs = {}
+        for vl in lhs.vertices:
+            for vr in rhs.vertices:
+                if vl.id == vr.id:
+                    lhs_to_rhs[vl] = vr
+        for el in lhs.edges:
+            if el.v1 in lhs_to_rhs and el.v2 in lhs_to_rhs:
+                v1 = lhs_to_rhs[el.v1]
+                v2 = lhs_to_rhs[el.v2]
+                is_good = False
+                for er in rhs.edges:
+                    if er.v1 == v1 and er.v2 == v2:
+                        is_good = True
+                        break
+                if is_good:
+                    lhs_to_rhs[el] = er
+
+        prune_edges = True    
+        if self.lookahead.type == TokenTypes.EXCLAMATION:
+            self._match(TokenTypes.EXCLAMATION)
+            prune_edges = False
+        self.productions.append(Rule(lhs,rhs,lhs_to_rhs,prune=prune_edges))
+
+            
+        #self.productions.append( Production(lhs, rhs) )
+       
+    #--------------------------------------------------------------------------
+    def _parseProductionList(self):
+        """
+        prod_list -> prod ';' prod_list | nil
+        
+        A list of zero or more productions separated by semicolons.
+        """
+        while self.lookahead.type == TokenTypes.ID:
+            self._parseProduction()
+            self._match(TokenTypes.SEMICOLON)
+
+    #--------------------------------------------------------------------------
+    def _parseProductions(self):
+        """
+        productions -> 'productions' '{' start_graph prod_list '}'
+
+        The productions section of the input file. Consists of a
+        'productions' keyword, and then a start graph and list of production 
+        statements surrounded by curly braces.
+        """
+        self._match(TokenTypes.PRODUCTIONS)
+        self._match(TokenTypes.LBRACE)
+        self._parseStartGraph()
+        self._parseProductionList()
+        self._match(TokenTypes.RBRACE)
+
+    #--------------------------------------------------------------------------
+    def _parseStartGraph(self):
+        """
+        start_graph -> graph ';'
+
+        A simple graph that the generator will use to initialize the
+        starting graphs.
+        """
+        self.startGraph = self._parseGraph()
+        self._match(TokenTypes.SEMICOLON)
+
+    #--------------------------------------------------------------------------
+    def _parseVertexID(self, token, graph):
+        """
+        Parses the given token (ID) into a text label and optional
+        vertex number (e.g., "A1"). If a vertex with these two data don't 
+        exist in the given graph, it is added. Otherwise, the existing 
+        vertex from the graph is returned.
+        """
+        label = re.match('[A-z]+', token.text).group(0)
+        match = re.search('[0-9]+$', token.text)
+        number = match.group(0) if match is not None else float('nan')
+        vertex = graph.find_vertex(number)
+        if vertex == None:
+            # graph doesn't contain a vertex with the label.
+            vertex = Vertex(
+                number,
+                label
+            )
+            graph.add_vertex(vertex)
+        return vertex
+ 
